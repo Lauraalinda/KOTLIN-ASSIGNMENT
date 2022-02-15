83ff2234ecf6831e461ca13800d2ff1cb5bd1025
Lauraalinda <lauraallindaa@gmail.com>
2022-02-15 22:03:06 +0300

    Kotlin assignment

---
+++ src/KOTLIN ASSIGNMENT.kt
@@ -0,0 +1,27 @@
+fun main (){
+    var names= arrayOf("lAURA ALINDA" ,"LONA CRYSTABEL" ,"KENYANA LINDA")
+    var age= arrayOf( 20 ,23 ,31)
+    var phoneNumber= arrayOf("072344822" ,"0772434325" ,"072345355")
+    var weight_in_kg=arrayOf("120" ,"98" ,"45")
+    var Kenyan=arrayOf("true" ,"false" ,"true")
+
+    //the names are organised from the first to the last//
+    println(names[0])
+    println(age[0])
+    println(phoneNumber[0])
+    println(weight_in_kg[0])
+    println(Kenyan[0])
+
+    println(names[1])
+    println(age[1])
+    println(phoneNumber[1])
+    println(weight_in_kg[1])
+    println(Kenyan[1])
+
+    println(names[2])
+    println(age[2])
+    println(phoneNumber[2])
+    println(weight_in_kg[2])
+    println(Kenyan[2])
+
+}
